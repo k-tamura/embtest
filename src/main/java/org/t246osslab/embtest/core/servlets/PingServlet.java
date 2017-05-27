daba37f1a4f2d2a672e9af6fa52b654ff013b2c6
k-tamura <ktamura.biz.80@gmail.com>
2017-05-27 09:10:02 +0900

    Add a test servlet and properties

---
+++ src/main/java/org/t246osslab/embtest/core/servlets/PingServlet.java
@@ -0,0 +1,36 @@
+package org.t246osslab.embtest.core.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = { "/ping" })
+public class PingServlet extends HttpServlet {
+    
+    private static final Logger log = LoggerFactory.getLogger(PingServlet.class);
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        PrintWriter writer = null;
+        try {
+            res.setContentType("text/plain");
+            writer = res.getWriter();
+            writer.write("It works!");
+        } catch (Exception e) {
+            log.error("Exception occurs: ", e);
+        } finally {
+            if(writer != null){
+                writer.close();
+            }
+        }
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,8 +1,22 @@
+<%@ page pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<c:set var="language"
+	value="${not empty param.language ? param.language : not empty language ? language : pageContext.request.locale}"
+	scope="session" />
+<fmt:setLocale value="${language}" />
+<fmt:setBundle basename="indexpage" />
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Test</title>
+<title>Uber Test</title>
 </head>
-<body>
-	<p>User-Agent: <%=request.getHeader("user-agent")%></p>
+<body style="margin-top: 20px; margin-left: 20px; margin-right: 20px;">
+	<ul>
+		<li><p>
+				<a href="ping">Call Ping Servlet</a>:
+				<fmt:message key="function.ping" />
+			</p></li>
+	</ul>
 </body>
 </html>
